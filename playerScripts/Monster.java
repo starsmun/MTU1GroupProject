bb8ccc4b6217a9a416a22a0329293d544821dd1f
Lachlan <21005784@massey.ac.nz>
2022-05-28 21:53:16 +1200

    Added Classes

--- playerScripts/Monster.java
+++ playerScripts/Monster.java
@@ -1,6 +1,60 @@
 package PlayerScripts;
 
-public class Monster extends BaseCreature{
-    int killPrize;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Monster extends BaseCreature{
+    int killPrize, maxLevel;
+    String name;
+
+    public void setup(String name) {
+        this.name = name;
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("PlayerScripts/MonsterStats.txt")));
+
+            String line;
+            String[] mDetails;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                mDetails = line.split(",");
+                if(Objects.equals(mDetails[0], this.name)){
+                    killPrize = Integer.parseInt(mDetails[1]);
+                    maxHealth = Integer.parseInt(mDetails[2]);
+                    defense = Integer.parseInt(mDetails[3]);
+                    attack = Integer.parseInt(mDetails[4]);
+                    maxLevel = Integer.parseInt(mDetails[5]);
+                    break;
+                }
+
+            }
+        } catch (
+            FileNotFoundException e) { // Catches when the file is not found
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getKillPrize(){
+        return killPrize;
+    }
+
+    public boolean attackCreature(Player player) {
+        boolean killedCheck = super.attackCreature(player);
+        if(killedCheck){ // If creature killed
+            System.out.println("Player is dead");
+
+        }
+        return killedCheck;
+    }
 }
