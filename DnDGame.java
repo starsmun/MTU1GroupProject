0292cb29bca6b596c9ab833032961fa8ed523c2e
Lachlan <21005784@massey.ac.nz>
2022-06-01 15:37:38 +1200

    Fixed Stuff

--- DnDGame.java
+++ DnDGame.java
@@ -13,7 +13,6 @@ public class DnDGame extends GameEngine{
     private static final List<CoolButton> monsterButtons = new LinkedList<>();
     private static int height = 600, width = 1000;
     private static float state = 1;
-    private static String itemTitle="", itemDescription="";
     private static Image background, player1, monster1, monster2, monster3, monster4;
 
     private static Player selectedPlayer;
@@ -86,7 +85,7 @@ public class DnDGame extends GameEngine{
             if(button.stateR == state) {
                 if(button.ID > 3 && button.ID < 8) {
                     try {
-                        onScreenItems.get(button.ID - 4);
+                        onScreenMonsters.get(button.ID - 4);
                         paintButton(button);
                     } catch (IndexOutOfBoundsException ignored) {
 
@@ -239,7 +238,7 @@ public class DnDGame extends GameEngine{
 
         else if(state == 1.1f){
             if(ID > 3 && ID < 8) {
-                selectedMonster = ManageCreatures.monsterByIndex(ID);
+                selectedMonster = ManageCreatures.monsterByIndex(ID-4);
                 if(condition == "press") selectedPlayer.attackCreature(selectedMonster);
             }else {
                 if(condition == "press") {
